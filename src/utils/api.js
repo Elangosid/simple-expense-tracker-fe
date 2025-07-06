3f568a91c861ba18e57f6801ad14f2315dbdcd14
sri sidhu <srisidhu@EZIO.COM>
2025-07-06 23:45:06 +0530

    backend url changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const userId = "user-123";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://sribackend.duckdns.org/api",
   headers: {
     "Content-Type": "application/json",
   },
